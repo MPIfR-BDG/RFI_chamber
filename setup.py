afae9f1579c6e68a702ddc5e7ea1c8f352765fc8
Ewan Barr <ewan.d.barr@googlemail.com>
2020-02-19 09:20:26 +0100

    added setup file

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(name='rfi_chamber',
+      version='1.0',
+      description='Python script for running spectrometer captures',
+      url='https://github.com/MPIfR-BDG/RFI_chamber',
+      author='Ewan Barr',
+      author_email='ebarr@mpifr-bonn.mpg.de',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'pyvisa',
+          'pyvisa-py'
+          'pyyaml',
+          'coloredlogs',
+          'astropy'
+      ],
+      dependency_links=[
+      ],
+      zip_safe=False)
